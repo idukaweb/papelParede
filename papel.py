3649565fadf7efc321bb4a47e14c783d237e2d21
iDukaWEB <idukaweb@gmail.com>
2022-02-02 13:23:40 -0300

    Algoritmo do script de Papel Parede

---
+++ papel.py
@@ -0,0 +1,14 @@
+#
+# Programa para calcular a metragem de papel parede
+# By MWebRJ
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# 1 - Cliente informa a quantidade de paredes a serem cobertas
+# 2 - Cliente informa a metragem (altura x comprimento) de cada parede
+# 3 - Vendedor informa os tipos de papéis disponíveis
+# 4 - Cliente escolhe o papel
+# 5 - Vendedor insere os dados de cada parede no sistema
+# 6 - Sistema identifica a altura e comprimento de cada tipo de papel usado
+# 7 - Sistema calcula a quantidade de papel para cada parede
+# 8 - Sistema apresenta relatório com quantidade de papel usado e o valor a ser pago
+
