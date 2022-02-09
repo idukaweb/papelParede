824a785cf01e0f9a7fcf39f9ac89190835c91cdb
iDukaWEB <idukaweb@gmail.com>
2022-02-09 14:31:25 -0300

    Testes iniciais

--- papel.py
+++ papel.py
@@ -6,6 +6,7 @@
 
 # - Importação de bibliotecas
 from math import ceil
+from tkinter import *
 
 
 # - Funções Utilizadas
@@ -23,17 +24,20 @@ def calc_area(par):
     
     print('\nTotal da área a ser coberta:')
     print(f'Altura: {alt:6.2f}m²\tLargura: {lar:6.2f}m²')
-
+    resposta = f'Altura: {alt:6.2f}m²\tLargura: {lar:6.2f}m²'
 # - Sistema calcula a área total a ser coberta
     area_total = alt * lar
     print(f'Área total: {area_total:5.2f}m²')
+    resposta += f'\nÁrea total: {area_total:5.2f}m²'
+    tela.num_paredes['text'] = resposta
 
 # - ---------------------------------------------------- HEADER
 # - Início da execução do programa
 # - Vendedor informa os tipos de papéis disponíveis
 lista_papeis = [[2.5, 0.5], [2.4, 0.75], [1.7, 0.5]]
 # - Sistema mostra a altura e comprimento de cada tipo de papel usado
-print(f'\n- Sistema de cálculo de papel de parede -')
+mensagem = f'\n- Sistema de cálculo de papel de parede -'
+print(mensagem)
 print(f'\nMetragem dos Papéis disponíveis')
 for i in lista_papeis:
     print(f'Tipo (alt|comp):{i}')
@@ -43,7 +47,6 @@ for i in lista_papeis:
 # - Cliente informa a quantidade de paredes a serem cobertas
 
 qtd_paredes = int(input("Informe a quantidade de paredes: "))
-
 paredes = []
 # - Cliente informa a metragem (altura x comprimento) de cada parede
 # - Vendedor insere os dados de cada parede no sistema
@@ -61,11 +64,11 @@ while i < qtd_paredes:
 
 
 # - Sistema mostra a metragem de cada parede
-print('Metragem cadastrada com sucesso \n->>',paredes)
+print('Metragem cadastrada com sucesso \n>>',paredes)
 
 # - Chamada da função de calcular a área a ser coberta
 
-calc_area(paredes)
+
 
 # - Sistema apresenta relatório com quantidade de papel usado e o valor a ser pago
 
@@ -73,3 +76,5 @@ calc_area(paredes)
     Calcular a área total, colocar 10% a mais de margem, dividir pela largura do papel escolhido
     Ou Calcular a área total e e dividir pela área do papel (acho que não dá certo o cálculo)
 '''
+
+# criar versão com GUI
